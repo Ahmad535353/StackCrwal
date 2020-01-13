ef44720eac394685cb8876672c41d221e8e5c10b
Ahmad <mohammadkhani.ahmad53@gmail.com>
2020-01-14 01:10:12 +0330

    Web

--- Crawl.py
+++ Crawl.py
@@ -1,16 +1,27 @@
 from stackapi import StackAPI
-import datetime, time
-today = datetime.date.today()
-week_ago = today - datetime.timedelta(days=7)
-today_sec = time.mktime(today.timetuple())
-week_ago_sec = time.mktime(week_ago.timetuple())
-SITE = StackAPI('stackoverflow')
-# tags = SITE.fetch('tags')
-questions = SITE.fetch('questions', fromdate=int(week_ago_sec), todate=int(today_sec),
-    tagged='Android', sort='creation')
-f = open("1.txt", "w", encoding='utf8')
-for question in questions['items']:
-    f.write(str(question))
-    f.write(str('\n'))
-f.close()
-print('done')
+import datetime
+import time
+
+
+def fetch_results():
+
+    today = datetime.date.today()
+    week_ago = today - datetime.timedelta(days=7)
+    today_sec = time.mktime(today.timetuple())
+    week_ago_sec = time.mktime(week_ago.timetuple())
+    SITE = StackAPI('stackoverflow')
+    # tags = SITE.fetch('tags')
+    questions = SITE.fetch('questions', fromdate=int(week_ago_sec), todate=int(today_sec),
+                           tagged='Android', sort='creation', min='10', filter='!9YdnSIN*P')
+    questions = questions['items'][:10]
+    # f = open("1.txt", "w", encoding='utf8')
+    # for question in questions['items']:
+    #     f.write(str(question))
+    #     f.write(str('\n'))
+    # f.close()
+    for key, value in questions[0].items():
+        print('hi')
+    print('Done fetching.')
+    return questions
+
+fetch_results()

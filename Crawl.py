a099a75ac901b686dabab6e79c2f81b38b2d3ecb
Ahmad <mohammadkhani.ahmad53@gmail.com>
2020-01-14 10:10:35 +0330

    Saved version added.

--- Crawl.py
+++ Crawl.py
@@ -5,7 +5,6 @@ import pickle
 
 
 def fetch_results(number_of_results, days):
-
     if number_of_results > 20:
         number_of_results = 20
     if days < 0 or number_of_results < 0:
@@ -20,7 +19,6 @@ def fetch_results(number_of_results, days):
     #     questions_by_vote = pickle.load(data)
     # return questions, questions_by_vote
 
-
     today = datetime.date.today()
     week_ago = today - datetime.timedelta(days=days)
     today_sec = time.mktime(today.timetuple()) + 86400
@@ -33,7 +31,7 @@ def fetch_results(number_of_results, days):
                            tagged='Android', sort='creation', filter='!9YdnSIN*P')
     questions = questions['items']
     questions_by_vote = SITE.fetch('questions', fromdate=int(week_ago_sec), todate=int(today_sec),
-                           tagged='Android', sort='votes', filter='!9YdnSIN*P')
+                                   tagged='Android', sort='votes', filter='!9YdnSIN*P')
     questions_by_vote = questions_by_vote['items']
 
     print('Done fetching.')
@@ -43,6 +41,7 @@ def fetch_results(number_of_results, days):
     #     pickle.dump(questions_by_vote, output)
 
     return questions, questions_by_vote
+
 
 number = 2
 days = 7
